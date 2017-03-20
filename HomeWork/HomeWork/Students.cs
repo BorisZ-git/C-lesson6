de55fbcdef7c9c6d67a8c0d9db53eec4caff154e
BorisZ-git <ribos@bk.ru>
2017-03-21 01:31:46 +0300

    set course for students\\ create list of Students

--- HomeWork/HomeWork/Students.cs
+++ HomeWork/HomeWork/Students.cs
@@ -16,11 +16,17 @@ namespace HomeWork
         {
             students = new int[100];
             age = new int[students.Length];
-            course = new int[6] { 1, 2, 3, 4, 5, 6 };
+            course = new int[students.Length];           
             for (int i =0;i< students.Length; i++)
             {
                 students[i] = i + 1;
                 age[i] = r.Next(18, 23);
+                if (age[i] == 18) course[i] = 1;
+                else if (age[i] == 19) course[i] = 2;
+                else if (age[i] == 20) course[i] = 3;
+                else if (age[i] == 21) course[i] = 4;
+                else if (age[i] == 22) course[i] = 5;
+                else if (age[i] == 23) course[i] = 6;
             }
         }
     }
