73f447af8b4da6ea0988885f5bbf229da4cee889
BorisZ-git <ribos@bk.ru>
2017-03-21 01:25:06 +0300

    start doing HomeWork for lesson 6\\ wrote class describe students params

---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Boris Z
+ * 3. Подсчитайте количество студентов:
+а) учащихся на 5 и 6 курсах;
+б)*подсчитайте сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся;
+в) отсортируйте список по возрасту студента
+г) *отсортируйте список по курсу и возрасту студента
+ * */
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students st = new Students();
+            for (int i = 0; i < st.students.Length; i++)
+                Console.WriteLine($"student: {st.students[i]} = age = {st.age[i]}");
+        }
+    }
+}
---
+++ HomeWork/HomeWork/Students.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Students
+    {
+        public int[] students;
+        public int[] age;
+        int[] course;
+        Random r = new Random();        
+        public Students()
+        {
+            students = new int[100];
+            age = new int[students.Length];
+            course = new int[6] { 1, 2, 3, 4, 5, 6 };
+            for (int i =0;i< students.Length; i++)
+            {
+                students[i] = i + 1;
+                age[i] = r.Next(18, 23);
+            }
+        }
+    }
+}
